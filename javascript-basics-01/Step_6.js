28bb147ce2ce465b8d6a6e9e34759bd06c4d78c1
Amen <jamen.jamne30@gmail.com>
2022-12-07 15:46:07 +0200

    basics-1-6 finished

--- javascript-basics-01/Step_6.js
+++ javascript-basics-01/Step_6.js
@@ -1,9 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Step 1</title>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-    </body>
-</html>
+const validateButton = document.querySelector("#validate");
+const firstNumInput = document.querySelector("#first_number");
+const secondNumInput = document.querySelector("#second_number");
+
+let num1 = parseInt(prompt("Please enter the first number"));
+let num2 = parseInt(prompt("Please enter the second number"));
+
+firstNumInput.value = num1;
+secondNumInput.value = num2;
+
+validateButton.addEventListener("click", (e)=>{
+    e.preventDefault();
+    alert ("The answer is: " + (num1 % num2));
+    firstNumInput.value = secondNumInput.value = "";
+})
