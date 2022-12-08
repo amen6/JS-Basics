9e7b2e0370065b6d8c1fa0894b12442f76df7ea3
Amen <jamen.jamne30@gmail.com>
2022-12-08 10:36:51 +0200

    basics-3-3 finished

---
+++ javascript-basics-03/step 3/script.js
@@ -0,0 +1,11 @@
+const colorDivs = document.querySelectorAll(".color")
+const p = document.querySelector("p")
+
+colorDivs.forEach((e)=> {
+    console.log(e)
+
+    e.addEventListener("click", ()=> {
+        let color = e.getAttribute("class").split(" ")[1]
+        p.style = `color: ${color}`;
+    })
+})
