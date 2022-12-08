4b1a9b5ca2cacdf13c0e4ae754cdcda126c975df
Amen <jamen.jamne30@gmail.com>
2022-12-08 20:29:43 +0200

    basics-2-4 fixed

--- javascript-basics-02/step 4/script.js
+++ javascript-basics-02/step 4/script.js
@@ -4,8 +4,9 @@ const cityInput = document.querySelector("#city")
 const button = document.querySelector("button")
 
 button.addEventListener("click", ()=> {
-    let answer = prompt("Do you want to clear all, please answer by 'yes/no'")
-    if (answer == "yes") {
+    let answer = confirm("Do you want to clear all?")
+    if (answer == true) {
         nameInput.value = surnameInput.value = cityInput.value = "";
     }
 })
+
